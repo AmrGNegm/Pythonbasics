ac809216cce60cd3f80549d49d7f4f93d92bbc0a
Amr Negm <amr.g.negm@gmail.com>
2020-11-25 23:23:38 +0200

    Is the number odd or even ? Using If Statment.
    #Excercies Num1 (Is the number odd or even?) Using If Statment.

---
+++ Is the number odd or even.py
@@ -0,0 +1,7 @@
+#Excercies Num1 (Is the number odd or even?) Using If Statment.
+x = int(input("Enter an integer: "))
+if x%2 == 0 :
+    print("The Number is Even")
+
+else :
+    print("The Number is odd")
