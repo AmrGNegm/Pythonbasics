35a473d06ef326290f26a4f93b82845eff9aa86b
Amr Negm <amr.g.negm@gmail.com>
2020-11-26 00:34:24 +0200

    Create What are the odd-even numbers from 2 to 15.py
    #Excercies Num2 (What are the odd/even numbers from 2 to 15) Using For Statment.

---
+++ What are the odd-even numbers from 2 to 15.py
@@ -0,0 +1,11 @@
+#Excercies Num2 (What are the odd numbers from 2 to 15) Using For Statment.
+n = 0
+for i in range (2,15,2):
+    n = i + 1
+    print(n)
+
+#Excercies Num2.1 (What are the even numbers from 2 to 15) Using For Statment.
+n = 0
+for i in range (2,15,2):
+    n = i 
+    print(n)
